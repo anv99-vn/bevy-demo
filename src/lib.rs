3b89270238dfb5c1a19fa95b9997d621f37838d9
anv99 <anv99-vn@users.noreply.github.com>
2026-08-01 19:53:32 +0700

    Build Android APK in release workflow

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+mod camera;
+mod cube;
+
+use bevy::prelude::*;
+
+// the `bevy_main` proc_macro generates the required boilerplate for Android
+#[bevy_main]
+pub fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Bevy 3D Demo".into(),
+                resolution: (800.0_f32, 600.0_f32).into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_systems(Startup, (cube::setup, camera::setup))
+        .add_systems(Update, (cube::rotate, camera::update))
+        .run();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,3 @@
-mod camera;
-mod cube;
-
-use bevy::prelude::*;
-
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Bevy 3D Demo".into(),
-                resolution: (800.0_f32, 600.0_f32).into(),
-                ..default()
-            }),
-            ..default()
-        }))
-        .add_systems(Startup, (cube::setup, camera::setup))
-        .add_systems(Update, (cube::rotate, camera::update))
-        .run();
+    bevy_demo::main();
 }
